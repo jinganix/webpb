66661152a4f22d08eb1b7ef869602c4175a013eb
gan.jin <jinganix@gmail.com>
2023-10-12 22:32:11 -0500

    chore: update license author and github link (#12)

--- sample/backend/src/test/java/io/github/jinganix/webpb/sample/backend/ApplicationTests.java
+++ sample/backend/src/test/java/io/github/jinganix/webpb/sample/backend/ApplicationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 jinganix@gmail.com, All Rights Reserved.
+ * Copyright (c) 2020 The Webpb Authors, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * https://github.com/jinganix/webpb
  */
 
 package io.github.jinganix.webpb.sample.backend;
@@ -20,20 +22,29 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mockStatic;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DisplayName("Application")
 class ApplicationTests {
 
-  @Test
-  void contextLoads() {
-    assertDoesNotThrow(Application::new);
-    try (MockedStatic<SpringApplication> application = mockStatic(SpringApplication.class)) {
-      assertNotNull(application);
-      Application.main(null);
+  @Nested
+  @DisplayName("when load context")
+  class WhenLoadContext {
+
+    @Test
+    @DisplayName("then load success")
+    void thenLoadSuccess() {
+      assertDoesNotThrow(Application::new);
+      try (MockedStatic<SpringApplication> application = mockStatic(SpringApplication.class)) {
+        assertNotNull(application);
+        Application.main(null);
+      }
     }
   }
 }
